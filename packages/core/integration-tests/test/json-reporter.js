e44456947346d249171b98b099a55be886b6deb2
Will Binns-Smith <wbinnssmith@atlassian.com>
2019-09-30 11:28:50 -0700

    JSONReporter test: use a sinon stub instead of patching console ourselves

--- packages/core/integration-tests/test/json-reporter.js
+++ packages/core/integration-tests/test/json-reporter.js
@@ -3,9 +3,12 @@
 /* eslint-disable no-console */
 
 import assert from 'assert';
+import invariant from 'assert';
 import path from 'path';
 import {bundle} from '@parcel/test-utils';
 import defaultConfigContents from '@parcel/config-default';
+// import semver from 'semver';
+import sinon from 'sinon';
 
 const jsonConfig = {
   ...defaultConfigContents,
@@ -14,12 +17,36 @@ const jsonConfig = {
 };
 
 describe('json reporter', () => {
+  let stub;
+  beforeEach(() => {
+    stub = sinon.stub(console, 'log');
+  });
+
+  afterEach(() => {
+    stub.restore();
+  });
+
   it('logs bundling a commonjs bundle to stdout as json', async () => {
-    let oldConsoleLog = console.log;
-    let i = 0;
-    // $FlowFixMe
-    console.log = function log(msg) {
-      let parsed = JSON.parse(msg);
+    // For some reason on CI, outside of Linux or Node 12, this asynchronous
+    // function resolves before it completes.
+    // if (
+    //   process.platform !== 'linux' &&
+    //   !semver.satisfies(process.version, '>=12')
+    // ) {
+    //   return;
+    // }
+
+    await bundle(path.join(__dirname, '/integration/commonjs/index.js'), {
+      defaultConfig: jsonConfig,
+      logLevel: 'info'
+    });
+
+    let parsedCalls = stub.getCalls().map(call => JSON.parse(call.lastArg));
+    for (let [iStr, parsed] of Object.entries(parsedCalls)) {
+      parsed = (parsed: any);
+      invariant(typeof iStr === 'string');
+      let i = parseInt(iStr, 10);
+
       if (i === 0) {
         assert.deepEqual(parsed, {type: 'buildStart'});
       } else if (i > 0 && i < 9) {
@@ -32,37 +59,23 @@ describe('json reporter', () => {
           phase: 'bundling'
         });
       } else if (i === 10) {
-        assert.deepEqual(parsed, {
-          type: 'buildProgress',
-          phase: 'packaging',
-          bundleFilePath: 'dist/index.js'
-        });
+        assert.equal(parsed.type, 'buildProgress');
+        assert.equal(parsed.phase, 'packaging');
+        assert(parsed.bundleFilePath.endsWith('dist' + path.sep + 'index.js'));
       } else if (i === 11) {
+        assert.equal(parsed.type, 'buildProgress');
+        assert.equal(parsed.phase, 'optimizing');
+        assert(parsed.bundleFilePath.endsWith('dist' + path.sep + 'index.js'));
+      } else if (i === 12) {
         assert.equal(parsed.type, 'buildSuccess');
         assert(typeof parsed.buildTime === 'number');
         assert(Array.isArray(parsed.bundles));
         let bundle = parsed.bundles[0];
-        assert.equal(bundle.filePath, 'dist/index.js');
+        assert(bundle.filePath.endsWith('dist' + path.sep + 'index.js'));
         assert(typeof bundle.size === 'number');
         assert(typeof bundle.time === 'number');
         assert(Array.isArray(bundle.largestAssets));
-      } else {
-        // $FlowFixMe
-        assert.fail(`Unexpected message ${msg} in position ${i}`);
       }
-
-      i++;
-    };
-
-    try {
-      await bundle(path.join(__dirname, '/integration/commonjs/index.js'), {
-        defaultConfig: jsonConfig
-      });
-    } catch (e) {
-      throw e;
-    } finally {
-      // $FlowFixMe
-      console.log = oldConsoleLog;
     }
   });
 });
